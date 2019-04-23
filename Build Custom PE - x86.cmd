882ddbe874f7e716afdb5934f48ae86ead55dcd4
Harold Mitts <HaroldMitts@users.noreply.github.com>
2019-04-23 09:34:23 -1000

    Sample command scripts to build custom WinPE images and add common Optional Components
    Optional Components help extend WinPE functionality so you can perform advanced proceedures like running PowerShell and other code which might not be natively supported in WinPE by default.

---
+++ Build Custom PE - x86.cmd
@@ -0,0 +1,23 @@
+copype x86 c:\tmp\winpe\x86-customPE
+
+dism /mount-image /imagefile:"c:\TMP\WinPE\x86-CustomPE\media\sources\boot.wim" /index:1 /mountdir:"C:\Mount\WinPE"
+
+dism /add-package /image:"C:\Mount\WinPE" /packagepath:"c:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\x86\WinPE_OCs\WinPE-WMI.cab"
+dism /add-package /image:"C:\Mount\WinPE" /packagepath:"c:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\x86\WinPE_OCs\en-us\WinPE-WMI_en-us.cab"
+dism /add-package /image:"C:\Mount\WinPE" /packagepath:"c:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\x86\WinPE_OCs\WinPE-Scripting.cab"
+dism /add-package /image:"C:\Mount\WinPE" /packagepath:"c:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\x86\WinPE_OCs\en-us\WinPE-Scripting_en-us.cab"
+dism /add-package /image:"C:\Mount\WinPE" /packagepath:"c:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\x86\WinPE_OCs\WinPE-NetFx.cab"
+dism /add-package /image:"C:\Mount\WinPE" /packagepath:"c:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\x86\WinPE_OCs\en-us\WinPE-NetFx_en-us.cab"
+dism /add-package /image:"C:\Mount\WinPE" /packagepath:"c:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\x86\WinPE_OCs\WinPE-PowerShell.cab"
+dism /add-package /image:"C:\Mount\WinPE" /packagepath:"c:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\x86\WinPE_OCs\en-us\WinPE-PowerShell_en-us.cab"
+dism /add-package /image:"C:\Mount\WinPE" /packagepath:"c:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\x86\WinPE_OCs\WinPE-DismCmdlets.cab"
+dism /add-package /image:"C:\Mount\WinPE" /packagepath:"c:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\x86\WinPE_OCs\en-us\WinPE-DismCmdlets_en-us.cab"
+
+
+dism /get-mountedimageinfo
+dism /image:"c:\Mount\WinPE" /get-pesettings
+dism /image:"c:\Mount\WinPE" /get-packages
+
+notepad C:\Mount\WinPE\Windows\System32\Startnet.cmd
+
+dism /unmount-image /mountdir:"C:\Mount\WinPE" /commit
